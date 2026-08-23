5e2319ef35b3b9b8651f3e749acb3cc9cb6a99c7
Andy Ly <59989200+LookItsAndy@users.noreply.github.com>
2026-08-23 14:27:52 -0700

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        # acm solution
+        result = []
+        path = []
+
+        def backtrack(start):
+            # base case
+            if len(path) == k:
+                result.append(path[:])
+            
+            for i in range(start, n + 1):   # 1, 2, 3, 4
+                path.append(i)
+                backtrack(i + 1)
+                path.pop()
+            # undo the choice
+
+        backtrack(1)
+        return result
